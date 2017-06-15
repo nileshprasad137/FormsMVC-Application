a9083d8221fde931b393a6b621a78f99ffe5e383
nileshprasad137 <prasadnilesh96@gmail.com>
2017-06-15 23:00:34 +0530

    source formatting

--- controllers/C_InsuranceCompany.class.php
+++ controllers/C_InsuranceCompany.class.php
@@ -6,91 +6,86 @@ require_once($GLOBALS['fileroot'] . "/library/classes/X12Partner.class.php");
 
 class C_InsuranceCompany extends Controller {
 
-	var $template_mod;
-	var $icompanies;
-                    
-                   var $form_action;
-                   var $style;
-                   var $current_action;
-                   var $webroot;
-                   
-                   var $x12_partners;
-                   var $insurancecompany;
-                   //var $icompanies;
-                   
-	function __construct($template_mod = "general") {
-		parent::__construct();
-		$this->icompanies = array();
-		$this->template_mod = $template_mod;
-		
-                                       $this->form_action =  $GLOBALS['webroot']."/controller.php?" . $_SERVER['QUERY_STRING'];
-                                       $this->current_action = $GLOBALS['webroot']."/controller.php?" . "practice_settings&insurance_company&";
-                                       $this->style = $GLOBALS['style'];
-                                       $this->webroot = $GLOBALS['webroot'] ;
-		$this->InsuranceCompany = new InsuranceCompany();
-	}
+    var $template_mod;
+    var $icompanies;
+    var $form_action;
+    var $style;
+    var $current_action;
+    var $webroot;
+    var $x12_partners;
+    var $insurancecompany;
 
-	function default_action() {
-		return $this->list_action();
-	}
+    //var $icompanies;
 
-	function edit_action($id = "",$patient_id="",$p_obj = null) {
-		if ($p_obj != null && get_class($p_obj) == "insurancecompany") {
-			$this->icompanies[0] = $p_obj;
-		}
-		elseif (get_class($this->icompanies[0]) != "insurancecompany" ) {
-			$this->icompanies[0] = new InsuranceCompany($id);
-		}
+    function __construct($template_mod = "general") {
+        parent::__construct();
+        $this->icompanies = array();
+        $this->template_mod = $template_mod;
 
-		$x = new X12Partner();		
-                                      $this->x12_partners = $x->_utility_array($x->x12_partner_factory());		
-                                      $this->insurancecompany = $this->icompanies[0];
-		
-                                        ob_start();
-                                        require_once($GLOBALS['template_dir'] . "insurance_companies/" . $this->template_mod . "_edit.php");
-                                        $echoed_content = ob_get_clean(); // gets content, discards buffer
-                                        return $echoed_content;
-	}
+        $this->form_action = $GLOBALS['webroot'] . "/controller.php?" . $_SERVER['QUERY_STRING'];
+        $this->current_action = $GLOBALS['webroot'] . "/controller.php?" . "practice_settings&insurance_company&";
+        $this->style = $GLOBALS['style'];
+        $this->webroot = $GLOBALS['webroot'];
+        $this->InsuranceCompany = new InsuranceCompany();
+    }
 
-	function list_action($sort = "") {
+    function default_action() {
+        return $this->list_action();
+    }
 
-		if (!empty($sort)) {			
-                                                                $this->icompanies = $this->InsuranceCompany->insurance_companies_factory("",$sort);
-		}
-		else {			
-                                                            $this->icompanies = $this->InsuranceCompany->insurance_companies_factory();
-		}		
-                                        ob_start();
-                                        require_once($GLOBALS['template_dir'] . "insurance_companies/" . $this->template_mod . "_list.php");
-                                        $echoed_content = ob_get_clean(); // gets content, discards buffer
-                                        return $echoed_content;
-	}
+    function edit_action($id = "", $patient_id = "", $p_obj = null) {
+        if ($p_obj != null && get_class($p_obj) == "insurancecompany") {
+            $this->icompanies[0] = $p_obj;
+        } elseif (get_class($this->icompanies[0]) != "insurancecompany") {
+            $this->icompanies[0] = new InsuranceCompany($id);
+        }
 
+        $x = new X12Partner();
+        $this->x12_partners = $x->_utility_array($x->x12_partner_factory());
+        $this->insurancecompany = $this->icompanies[0];
 
-	function edit_action_process() {
-                                    
-                                        //echo "hi";
-                                        if ($_POST['process'] != "true"){
-                                            //echo "failed";
-                                            return;                                        
-                                        }
-		//print_r($_POST);
-		if (is_numeric($_POST['id'])) {
-			$this->icompanies[0] = new InsuranceCompany($_POST['id']);
-		}
-		else {
-			$this->icompanies[0] = new InsuranceCompany();
-		}
+        ob_start();
+        require_once($GLOBALS['template_dir'] . "insurance_companies/" . $this->template_mod . "_edit.php");
+        $echoed_content = ob_get_clean(); // gets content, discards buffer
+        return $echoed_content;
+    }
 
-  		parent::populate_object($this->icompanies[0]);
+    function list_action($sort = "") {
 
-		$this->icompanies[0]->persist();
-		$this->icompanies[0]->populate();
+        if (!empty($sort)) {
+            $this->icompanies = $this->InsuranceCompany->insurance_companies_factory("", $sort);
+        } else {
+            $this->icompanies = $this->InsuranceCompany->insurance_companies_factory();
+        }
+        ob_start();
+        require_once($GLOBALS['template_dir'] . "insurance_companies/" . $this->template_mod . "_list.php");
+        $echoed_content = ob_get_clean(); // gets content, discards buffer
+        return $echoed_content;
+    }
 
-		//echo "action processeed";
-		$_POST['process'] = "";
-		header('Location:'.$GLOBALS['webroot']."/controller.php?" . "practice_settings&insurance_company&action=list");//Z&H
-	}
+    function edit_action_process() {
+
+        //echo "hi";
+        if ($_POST['process'] != "true") {
+            //echo "failed";
+            return;
+        }
+        //print_r($_POST);
+        if (is_numeric($_POST['id'])) {
+            $this->icompanies[0] = new InsuranceCompany($_POST['id']);
+        } else {
+            $this->icompanies[0] = new InsuranceCompany();
+        }
+
+        parent::populate_object($this->icompanies[0]);
+
+        $this->icompanies[0]->persist();
+        $this->icompanies[0]->populate();
+
+        //echo "action processeed";
+        $_POST['process'] = "";
+        header('Location:' . $GLOBALS['webroot'] . "/controller.php?" . "practice_settings&insurance_company&action=list"); //Z&H
+    }
 
 }
 
